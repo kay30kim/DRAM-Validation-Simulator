8e0eca3e8bc2ae2219fd59fb43b5e7c89fa7d523
Kay Kim <kay30kim@gmail.com>
2026-07-20 17:26:52 -0700

    Connect the core files into UEFI app

--- core/plat.h
+++ core/plat.h
@@ -8,7 +8,7 @@
  * core는 OS 함수(calloc/free 등)를 직접 부르지 않고 이 인터페이스만 쓴다.
  * 구현은 빌드 대상에 따라 갈아 끼운다:
  *   - host/plat_host.c : libc (calloc/free)
- *   - uefi/plat_uefi.c : UEFI Boot Services (AllocateZeroPool/FreePool, 예정)
+ *   - uefi/DramTestPkg/DramTestApp/plat_uefi.c : UEFI Boot Services (AllocatePool/FreePool)
  */
 void *plat_alloc_zero(size_t bytes);
 void plat_free(void *ptr);
---
+++ uefi/DramTestPkg/DramTestApp/core_dram_model.c
@@ -0,0 +1,3 @@
+// EDK2 can't reference sources outside the module folder, so we include an
+// exact copy of the host core file and compile it for UEFI.
+#include "../../../core/dram_model.c"
