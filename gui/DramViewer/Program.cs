464c747b7dbd934ca40a327169404d6f23707ef4
Kay Kim <kay30kim@gmail.com>
2026-07-28 07:44:50 -0700

    Simplify gui

--- gui/DramViewer/Program.cs
+++ gui/DramViewer/Program.cs
@@ -8,19 +8,11 @@ class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        AppBuilder builder = BuildAvaloniaApp();
-        builder.StartWithClassicDesktopLifetime(args);
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
     public static AppBuilder BuildAvaloniaApp()
     {
-        AppBuilder builder = AppBuilder.Configure<App>();
-        builder = builder.UsePlatformDetect();
-#if DEBUG
-        builder = builder.WithDeveloperTools();
-#endif
-        builder = builder.WithInterFont();
-        builder = builder.LogToTrace();
-        return builder;
+        return AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont();
     }
 }
